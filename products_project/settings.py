6aa7b6693d895fe0347378501eae21f1274fd310
Micah <airrenno@yahoo.com>
2022-12-27 08:20:16 -0600

    Created Local_Setting file and updated info.

--- products_project/settings.py
+++ products_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qxuop&3rc%i$)+i^sdwc2$$6+dv!0u0y=x*wiw#(p3k4(c4)_z'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -68,17 +65,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'products_project.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
